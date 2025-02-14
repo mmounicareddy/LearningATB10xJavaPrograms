f390a0588c3cc0e5a88ccd9faadb6ff58ed78e1b
Mounica m <mounica.reddy@zinier.com>
2025-02-15 01:32:27 +0530

    my 2nd commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello Mounica my java test program and git push");
+        System.out.println("hello Mounica my java test program and git push testing");
     }
 }
+
