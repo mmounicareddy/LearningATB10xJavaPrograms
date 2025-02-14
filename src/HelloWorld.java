018473feab30170b8e00e99aea7715e0aac41083
Mounica m <mounica.reddy@zinier.com>
2025-02-15 00:58:44 +0530

    My first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello Mounica");
+        System.out.println("hello Mounica my java test program");
     }
 }
